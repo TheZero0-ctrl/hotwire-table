19dd9520ccde8646c8d0b364f63ea2bde4a5f509
TheZero0-ctrl <ankitpariyar486@gmail.com>
2023-10-18 07:59:31 +0545

    revise: add item per page selection

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,6 @@
 class EmployeesController < ApplicationController
   def index
-    @pagy, @employees = pagy(::Employees::Find.new(Employee.all, params).execute, items: 10)
+    @items_per_page = params[:items_per_page] || 10
+    @pagy, @employees = pagy(::Employees::Find.new(Employee.all, params).execute, items: @items_per_page)
   end
 end
